c17bf98e8eab14b2dda9f699ec899a22f5a5465e
ktmgw486 <ktmgw486@gmail.com>
2025-12-12 08:03:35 +0000

    fix : prisma/schema.prisma delete

--- repositories/prisma/migrations/20251209131203_add_style_password_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `password` to the `style` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "style" ADD COLUMN     "password" TEXT NOT NULL;
