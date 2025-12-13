827aa829915a53d971e92a71cadb5b61a9f2c28a
CodeinHyuk <jih34602050@gmail.com>
2025-12-13 13:12:52 +0000

    fix: PR리뷰 반영 - curation.repository.js 함수 arrow to prisma로 변경

--- repository/curation.repository.js
+++ repository/curation.repository.js
@@ -1,8 +1,8 @@
 import { prisma, throwHttpError } from "./prisma/prisma.js";
 
 class CurationRepository {
-  async create(createData) {
-    return throwHttpError(prisma.curation.create, {
+  create = (createData) =>
+    throwHttpError(prisma.curation.create, {
       data: {
         ...createData,
         style: { connect: { id: createData.style_id } },
@@ -12,34 +12,33 @@ class CurationRepository {
         style: { select: { id: true, title: true } },
       },
     });
-  }
 
-  async update(curationId, password, updateData) {
-    return throwHttpError(prisma.curation.update, {
+  update = (curationId, password, updateData) =>
+    throwHttpError(prisma.curation.update, {
       where: {
         id_password: { id: curationId, password: password },
       },
       data: updateData,
     });
-  }
-  async delete(curationId, password) {
-    await throwHttpError(prisma.curation.delete, {
+
+  delete = (curationId, password) =>
+    throwHttpError(prisma.curation.delete, {
       where: {
         id_password: { id: curationId, password: password },
       },
     });
-  }
-  async findManyAndCount(where, pageSize, skip) {
-    const [totalItemCount, entities] = await Promise.all([
-      prisma.curation.count({ where }),
-      prisma.curation.findMany({
-        where,
-        take: pageSize,
-        skip: skip,
-        orderBy: { id: "desc" },
-      }),
-    ]);
-    return { totalItemCount, entities };
-  }
+
+  findManyAndCount = (where, pageSize, skip) =>
+    prisma
+      .$transaction([
+        prisma.curation.count({ where }),
+        prisma.curation.findMany({
+          where,
+          take: pageSize,
+          skip: skip,
+          orderBy: { id: "desc" },
+        }),
+      ])
+      .then(([totalItemCount, entities]) => ({ totalItemCount, entities }));
 }
 export const curationRepository = new CurationRepository();
