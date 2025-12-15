b782fe6921c8c9000e4492e55d01640654b65602
healimk <243699296+hyelimk@users.noreply.github.com>
2025-12-15 23:52:55 +0000

    fix:comment.repository.js

--- repository/comment.repository.js
+++ repository/comment.repository.js
@@ -1,42 +1,24 @@
-import { prisma, throwHttpError } from "./prisma/prisma.js";
-import { ForbiddenError, NotFoundError } from "../error/errors.js";
+import { prisma, throwHttpError } from "../repository/prisma/prisma.js";
 
-export const curationCommentRepository = {
-  create({ curationId, nickname, content, password }) {
-    return throwHttpError(prisma.curation_comment.create, {
-      data: {
-        nickname,
-        content,
-        password,
-        curation_id: curationId,
-      },
-    });
-  },
+export const create = (curationId, data) =>
+  throwHttpError(prisma.curationComment.create, {
+    data: {
+      ...data,
+      curation_id: curationId,
+    },
+  });
 
-  async update({ commentId, password, data }) {
-    const existing = await prisma.curation_comment.findUnique({
-      where: { id: commentId },
-    });
-    if (!existing) throw new NotFoundError("존재하지 않습니다");
-    if (existing.password !== password)
-      throw new ForbiddenError("비밀번호가 틀렸습니다");
+export const update = (commentId, password, data) =>
+  throwHttpError(prisma.curationComment.update, {
+    where: {
+      id_password: { id: commentId, password },
+    },
+    data,
+  });
 
-    return prisma.curation_comment.update({
-      where: { id: commentId },
-      data,
-    });
-  },
-
-  async delete({ commentId, password }) {
-    const existing = await prisma.curation_comment.findUnique({
-      where: { id: commentId },
-    });
-    if (!existing) throw new NotFoundError("존재하지 않습니다");
-    if (existing.password !== password)
-      throw new ForbiddenError("비밀번호가 틀렸습니다");
-
-    return prisma.curation_comment.delete({
-      where: { id: commentId },
-    });
-  },
-};
+export const remove = (commentId, password) =>
+  throwHttpError(prisma.curationComment.delete, {
+    where: {
+      id_password: { id: commentId, password },
+    },
+  });
