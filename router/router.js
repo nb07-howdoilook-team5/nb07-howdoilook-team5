3ca9f9390d8d396dc22d593478195fd859a36c5d
healimk <243699296+hyelimk@users.noreply.github.com>
2025-12-17 01:47:58 +0000

    add image router/controller

--- router/router.js
+++ router/router.js
@@ -1,18 +1,18 @@
 import { Router } from "express";
-// import commentRouter from "./comment.router.js";
-// import curationRouter from "./curation.router.js";
-// import styleRouter from "./style.router.js";
+import commentRouter from "./comment.router.js";
+import curationRouter from "./curation.router.js";
+import styleRouter from "./style.router.js";
 import imageRouter from "./image.router.js";
 
 const router = new Router();
 
-//router.use("/comments", commentRouter);
-// router.use("/curations", curationRouter);
-// router.use("/styles", styleRouter);
+router.use("/comments", commentRouter);
+router.use("/curations", curationRouter);
+router.use("/styles", styleRouter);
 router.use("/images", imageRouter);
 
-// router.get(`/tags`, getGalleryTags);
-// router.get(`/ranking`, getRankingStyles);
+router.get(`/tags`, getGalleryTags);
+router.get(`/ranking`, getRankingStyles);
 
 export default router;
 
