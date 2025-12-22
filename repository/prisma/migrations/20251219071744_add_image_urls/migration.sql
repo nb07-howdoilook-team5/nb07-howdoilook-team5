f6b74f638e03b3451f04ecaa5082424550ca7bba
CodeinHyuk <jih34602050@gmail.com>
2025-12-22 02:09:50 +0000

    fix: image 스키마 추가 및 임포트 경로 수정

---
+++ repository/prisma/migrations/20251219071744_add_image_urls/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "style" ADD COLUMN     "imageUrls" TEXT[];
--- router/router.js
+++ router/router.js
@@ -3,6 +3,8 @@ import commentRouter from "./comment.router.js";
 import curationRouter from "./curation.router.js";
 import styleRouter from "./style.router.js";
 import imageRouter from "./image.router.js";
+import styleController from "../controller/style.controller.js";
+import withErrorHandler from "../utils/asyncHandler.js";
 
 const router = new Router();
 
@@ -11,7 +13,6 @@ router.use("/curations", curationRouter);
 router.use("/styles", styleRouter);
 router.use("/images", imageRouter);
 
-router.get(`/tags`, getGalleryTags);
-router.get(`/ranking`, getRankingStyles);
+router.get("/ranking", withErrorHandler(styleController.getRankingStyles));
 
 export default router;
