da711debc4c1456b07d6306f3cadff72a7456798
ktmgw486 <ktmgw486@gmail.com>
2025-12-23 04:11:23 +0000

    fix: style final 수정완료

--- router/router.js
+++ router/router.js
@@ -4,6 +4,8 @@ import curationRouter from "./curation.router.js";
 import styleRouter from "./style.router.js";
 import imageRouter from "./image.router.js";
 import tagRouter from "./tag.router.js";
+import styleController from "../controller/style.controller.js";
+import withErrorHandler from "../utils/asyncHandler.js";
 
 const router = new Router();
 
@@ -12,7 +14,7 @@ router.use("/curations", curationRouter);
 router.use("/styles", styleRouter);
 router.use("/images", imageRouter);
 
-router.get(`/tags`, tagRouter);
-router.get(`/ranking`, getRankingStyles);
+router.use(`/tags`, tagRouter);
+router.get("/ranking", withErrorHandler(styleController.getRankingStyles));
 
 export default router;
